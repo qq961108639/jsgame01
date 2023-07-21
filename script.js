888d273230e507a592ccb9d5a994fceb18b31c71
CodeUX <qq961108639@icloud.com>
2023-07-21 13:46:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,15 +11,31 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let currentScore;
+let activePlayer;
+let playing;
+let scores;
 
-//变量 NOTE:
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//init game
+const init = function () {
+  //初始化分数
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0EL.textContent = 0;
+  current1EL.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player0El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player0El.classList.remove('player--active');
+};
+init(); //初始化执行
 
 //Switchplayer
 const switchPlayer = function () {
@@ -80,3 +96,5 @@ btnHold.addEventListener('click', function () {
 });
 
 //完成
+
+btnNew.addEventListener('click', init);
